efabe690484745cae25ea413ebfbd4404ce29cde
GermanM22 <german.escandon@hotmail.com>
2022-09-01 11:56:25 -0500

    Ejercicio1

---
+++ Ejercicio1/Ejercicio uno/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_uno
+{
+    internal class Medico:Entidad
+    {
+        public string FirstName { get; private set; }
+
+        public Medico(string name, string firstName) :base(name)
+        {
+            FirstName = firstName;
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio uno/Program.cs
@@ -0,0 +1,9 @@
+﻿using Ejercicio_uno;
+
+//polimorfismo
+Medico m = new Medico("Sebastian", "Velez");
+
+Paciente p = new Paciente("Andres", "Telemedicina");
+
+Console.WriteLine(m.Name);
+Console.WriteLine(p.Name);
